44b31c4103713826fd43d61d394239330ac402ff
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-07-21 21:15:08 +0200

    chore: bumped library version

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "com.tecknobit.kmprefs"
-version = "1.2.0"
+version = "1.2.1"
 
 kotlin {
     android {
@@ -124,7 +124,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.n7ghtm4r3",
         artifactId = "kmprefs",
-        version = "1.2.0"
+        version = "1.2.1"
     )
     pom {
         name.set("KMPrefs")
